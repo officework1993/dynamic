31214cbf7ff63638a5464d314602d56173993530
inder <chahal.inder0007@gmail.com>
2020-03-23 05:36:30 +0530

    changed run command to a file

---
+++ run.py
@@ -0,0 +1,6 @@
+
+import echonet
+
+echonet.utils.segmentation.run(modelname=\"deeplabv3_resnet50\",
+                                                    save_segmentation=True,
+                                                    pretrained=False)
