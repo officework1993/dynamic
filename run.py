984fb71dc2489130b18d5b106245492cbf6edd13
inder <chahal.inder0007@gmail.com>
2020-03-25 00:05:07 +0530

    removed redundant files

--- run.py
+++
@@ -1,6 +0,0 @@
-
-import echonet
-
-echonet.utils.segmentation.run(modelname=\"deeplabv3_resnet50\",
-                                                    save_segmentation=True,
-                                                    pretrained=False)
